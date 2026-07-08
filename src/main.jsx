f43cb7be2ab712f6170469e72b7a0b9c4fa402ee
tanphat9212-a11y <tanphat9212@gmail.com>
2026-07-08 10:47:37 +0700

    k

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,23 @@
+import {
+    AuthProvider
+}
+from "./context/AuthContext";
+
+
+import {
+    OrderProvider
+}
+from "./context/OrderContext";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { HelmetProvider } from "react-helmet-async";
+
+import { AuthProvider } from "./context/AuthContext";
+import { CartProvider } from "./context/CartContext";
+import { OrderProvider } from "./context/OrderContext";
+
+import AppRouter from "./router/AppRouter";
 
 import "./assets/css/reset.css";
 import "./assets/css/variables.css";
@@ -8,12 +25,19 @@ import "./assets/css/global.css";
 import "./assets/css/layout.css";
 import "./assets/css/responsive.css";
 
-const root = document.getElementById("root");
+import "./components/Common/Common.css";
+import "./components/Floating/Floating.css";
 
-if (root) {
-  ReactDOM.createRoot(root).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <OrderProvider>
+        <AuthProvider>
+          <CartProvider>
+            <AppRouter />
+          </CartProvider>
+        </AuthProvider>
+      </OrderProvider>
+    </HelmetProvider>
+  </React.StrictMode>
+);
