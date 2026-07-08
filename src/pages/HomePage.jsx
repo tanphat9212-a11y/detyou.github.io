93a97c732bb39547226a391d9fbd2611f558d178
tanphat9212-a11y <tanphat9212@gmail.com>
2026-07-08 10:08:58 +0700

    k

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,15 @@
 import Header from "../components/Header/Header";
 import Banner from "../components/Banner/Banner";
+import Category from "../components/Category/Category";
 
 export default function HomePage() {
   return (
     <>
       <Header />
-
       <Banner />
+      <Category />
 
-      <main className="container">
-
-      </main>
+      <main className="container"></main>
     </>
   );
 }
