02c035137415717dfa452ac92fd08ad87844887a
tanphat9212-a11y <tanphat9212@gmail.com>
2026-07-08 10:05:46 +0700

    k

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,35 @@
+import "./Banner.css";
+
+export default function Banner() {
+  return (
+    <section className="banner">
+
+      <div className="container">
+
+        <div className="banner-left">
+
+          <img
+            src="https://cdnv2.tgdd.vn/mwg-static/tgdd/Banner/7d/1d/7d1dc2d4f2d1d7d8d27d70d2a6e09b43.png"
+            alt=""
+          />
+
+        </div>
+
+        <div className="banner-right">
+
+          <img
+            src="https://cdnv2.tgdd.vn/mwg-static/tgdd/Banner/e8/7d/e87dba5b9c7fd1c6c9314df8c5a18b63.png"
+            alt=""
+          />
+
+          <img
+            src="https://cdnv2.tgdd.vn/mwg-static/tgdd/Banner/c2/8b/c28b7e1c2dd7ce1c23d2cb0fc1efad3d.png"
+            alt=""
+          />
+
+        </div>
+
+      </div>
+    </section>
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
 import Header from "../components/Header/Header";
+import Banner from "../components/Banner/Banner";
 
 export default function HomePage() {
   return (
     <>
       <Header />
+
+      <Banner />
 
       <main className="container">
 
