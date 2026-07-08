b7800fff044c1bec630496bb7622f3c30f1f94ae
tanphat9212-a11y <tanphat9212@gmail.com>
2026-07-08 10:28:03 +0700

    k

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,24 @@
+import {
+    AuthProvider
+}
+from "./context/AuthContext";
+
+
+import {
+    OrderProvider
+}
+from "./context/OrderContext";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App";
+import {
+    HelmetProvider
+}
+from "react-helmet-async";
+
+
+import AppRouter from "./router/AppRouter";
+
 
 import "./assets/css/reset.css";
 import "./assets/css/variables.css";
@@ -9,8 +26,49 @@ import "./assets/css/global.css";
 import "./assets/css/layout.css";
 import "./assets/css/responsive.css";
 
+
+import "./components/Common/Common.css";
+import "./components/Floating/Floating.css";
+
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+    <OrderProvider>
+
+        <AuthProvider>
+
+            <CartProvider>
+
+                <AppRouter />
+
+            </CartProvider>
+
+        </AuthProvider>
+
+    </OrderProvider>
+
 );
+
+import {
+    CartProvider
+}
+from "./context/CartContext";
+import {
+    AuthProvider
+}
+from "./context/AuthContext";
+
+<HelmetProvider>
+
+    <AuthProvider>
+
+        <CartProvider>
+
+            <AppRouter />
+
+        </CartProvider>
+
+    </AuthProvider>
+
+</HelmetProvider>
